56b86e9937b18881d58120edf4f5008491585aad
lucassosa23 <luucassosa4@gmail.com>
2022-06-23 17:19:41 -0300

    add sass a las columnas

--- src/App.js
+++ src/App.js
@@ -7,6 +7,31 @@ function App() {
      <div className='title'>
        <h1>Lucas Sosa</h1>
      </div>
+     <Row className='todo'>
+      <Col className='todo__title'
+      xs={{span: 10, offset: 1}}
+      md={{span: 6, offset: 3}}>
+        <h2>
+          Today
+        </h2>
+      </Col>
+
+      <Col className='todo__list'
+      xs={{span: 10, offset: 1}}
+      md={{span: 6, offset: 3}}>
+
+        <p>Lista de tareas</p>
+      </Col>
+      <Col className='todo__input'
+      xs={{span: 10, offset: 1}}
+      md={{span: 6, offset: 3}}>
+
+      <input placeholder='Nueva Tarea'/>
+      
+      </Col>
+
+
+     </Row>
    </Container>
   );
 }
