09d61f4b243f9917c207990e40390db547fce5b5
lucassosa23 <luucassosa4@gmail.com>
2022-06-23 16:58:07 -0300

    haciendo la estructura

---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ2ISNWVv7B19XSbIqbTMrEeGvGc3fTco",
+    authDomain: "app-list-276fe.firebaseapp.com",
+    projectId: "app-list-276fe",
+    storageBucket: "app-list-276fe.appspot.com",
+    messagingSenderId: "599429148378",
+    appId: "1:599429148378:web:e086920ce2de3170dabfe3"
+  };
+
+
+
+  export default firebase.initializeApp(firebaseConfig)
